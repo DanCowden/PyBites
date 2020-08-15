f5cee09a4f6e487e7caadbe753d2b2ad9d291b83
Daniel Cowden <dancowden@me.com>
2020-08-15 12:06:46 -0700

    Using variable instead of value.

--- 101/driving.py
+++ 101/driving.py
@@ -2,7 +2,7 @@ MIN_DRIVING_AGE = 18
 
 
 def allowed_driving(name, age):
-    if age >= 18:
+    if age >= MIN_DRIVING_AGE:
         print(f'{name} is allowed to drive')
     else:
         print(f'{name} is not allowed to drive')
