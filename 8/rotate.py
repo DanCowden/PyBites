e0e3c270d6ace9914c21f13af3641a697dcf6046
Daniel Cowden <dancowden@me.com>
2020-09-02 01:27:36 -0700

    Initial config of rotate.py

--- 8/rotate.py
+++ 8/rotate.py
@@ -1,5 +1,19 @@
 def rotate(string, n):
     """Rotate characters in a string.
        Expects string and n (int) for number of characters to move.
+
+       If n is positive move characters from beginning to end
+       e.g.: rotate('hello', 2) would return llohe
+       if n is negative move characters to the start of the string
+       e.g.: rotate('hello', -2) would return lohel
     """
-    pass
+
+    return string
+
+
+if __name__ == '__main__':
+    string = 'hello'
+    n = 2
+
+    test = rotate(string, n)
+    print(test)
