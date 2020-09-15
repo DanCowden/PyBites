ae42cb20a83cbb88d5cdcc3bd44f1b8d767cfbfb
Daniel Cowden <dancowden@me.com>
2020-09-14 23:47:25 -0700

    submission Bite 32 @ codechalleng.es

--- 32/inventory.py
+++ 32/inventory.py
@@ -6,12 +6,15 @@ The tests fail. Can you fix it?
 This can be done in a one liner.
 """
 
+from copy import deepcopy
+
 items = [{'id': 1, 'name': 'laptop', 'value': 1000},
          {'id': 2, 'name': 'chair', 'value': 300},
          {'id': 3, 'name': 'book', 'value': 20}]
 
 
 def duplicate_items(items):
+    items = deepcopy(items)
     return items[:]
 
 
